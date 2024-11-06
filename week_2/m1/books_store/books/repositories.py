a06ea355ba890bb6c69f87600298ace29d6f7e96
zemgh <bast3ran@yandex.ru>
2024-11-06 21:41:26 +0300

    select

--- week_2/m1/books_store/books/repositories.py
+++ week_2/m1/books_store/books/repositories.py
@@ -1,12 +1,16 @@
+from django.db import transaction
+
 from .models import Book
 
 
 class BookRepository:
     def buy_book(self, book: Book, value: int = 1, min_count: int = 0) -> bool:
+        with transaction.atomic():
+            book = Book.objects.select_for_update().get(id=book.id)
 
-        if book.count - value >= min_count:
-            book.count -= value
-            book.save()
-            return True
+            if book.count - value >= min_count:
+                book.count -= value
+                book.save()
+                return True
 
-        return False
+            return False
